f0e983c523c21fd60d70d0319b02c40cf47e20af
Omas Abdullah <omas.abdullah@gmail.com>
2013-04-01 20:42:35 -0400

    Begin handling for gameState
    Added 3 gamestates, NOT_STARTED, IN_PROGRESS, GAME_OVER. To handle
    serverside how game is run.

--- src/serverGameHandler.java
+++ src/serverGameHandler.java
@@ -3,13 +3,21 @@ import java.util.*;
 
 public class serverGameHandler extends Thread
 {
+	static final int NOT_STARTED = 0;
+	static final int IN_PROGRESS = 1;
+	static final int GAME_OVER = 2;
+	
+	
     Vector<String[]> receivedStrings = new Vector<String[]>();
+    int gameState = NOT_STARTED;
 	
 	public serverGameHandler(Vector<String[]> myString) throws IOException
 	{
 		this("serverGameHandler");
 		System.out.println("GameHandler Running...");
+
 		receivedStrings = myString;
+		
 	}
 	
 	public serverGameHandler(String name) throws IOException
@@ -19,6 +27,23 @@ public class serverGameHandler extends Thread
 	
 	public void run()
     {
-		
+		switch (gameState)
+		{
+			// Preparing game puzzle
+			case NOT_STARTED:
+			{
+	
+			} break;
+			// Game in Progress
+			case IN_PROGRESS:
+			{
+				
+			} break;
+			// Assign winner, reset scores, etc.
+			case GAME_OVER:
+			{
+				
+			} break;
+		}
     }
 }
